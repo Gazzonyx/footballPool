3389a7dae33b23da511bbb942f39fb69e26331dc
Scott Lovenberg <scott.lovenberg@gmail.com>
2013-10-15 19:53:25 -0400

    Added removePlayer method to remove a player.  Also added a test to doTest for it. Added a static variable for invalid players.

--- src/footballpool/core/database/DatabaseConnector.java
+++ src/footballpool/core/database/DatabaseConnector.java
@@ -17,6 +17,7 @@ public class DatabaseConnector implements Testable
 {
     private Connection connection;
     private final Object connectionLock = new Object();
+    public final static int INVALID_PLAYER_ID = -1;
     
     
     /**
@@ -49,7 +50,7 @@ public class DatabaseConnector implements Testable
     public Player getPlayer(String emailAddress) throws SQLException
     {
         if (emailAddress == null)
-            return new Player(-1);
+            return new Player(INVALID_PLAYER_ID);
         
         ResultSet rs;
         Player outgoing;
@@ -68,7 +69,7 @@ public class DatabaseConnector implements Testable
            if (!rs.next())
            {
                rs.close();
-               return new Player(-1);
+               return new Player(INVALID_PLAYER_ID);
            }
            
            outgoing = new Player(rs.getInt("id"), rs.getString("firstName"), 
@@ -98,7 +99,8 @@ public class DatabaseConnector implements Testable
         
         try
         {
-            if (getPlayer(email).id != -1)
+            // Check to see if player alread exists.  This should actually be an Exception.
+            if (getPlayer(email).id != INVALID_PLAYER_ID)
                 return false;
             
             PreparedStatement statement;
@@ -122,11 +124,60 @@ public class DatabaseConnector implements Testable
         
         return true;
     }
+    
+    
+    /**
+     * Removes a Player from the database.  AKA, they are deleted.  Player picks are also removed.
+     * This method is thread safe.
+     * @param emailAddress Email address of the player to be removed.
+     * @return True if player is found and deleted, false otherwise.
+     */
+    public boolean removePlayer(String emailAddress) throws SQLException
+    {
+        if (emailAddress == null || emailAddress.equals(""))
+            return false;
+        
+        Player player = null;
+        try
+        {
+            player = getPlayer(emailAddress);
+            
+            // See if they exist
+            if(player.id == INVALID_PLAYER_ID)
+                return false;
+            
+            
+            PreparedStatement statement;
+            synchronized(connectionLock)
+            {
+                statement = connection.prepareStatement(
+                    "DELETE FROM players WHERE id=" + player.id);
+                statement.execute();
+                
+                statement = connection.prepareStatement(
+                    "DELETE FROM picks WHERE playerID=" + player.id);
+                statement.execute();
+            }            
+        }
+        catch (SQLException se)
+        {
+            throw se;
+        }
+        finally
+        {
+            // This isn't really needed, but it makes me sleep well.
+            player = null;
+        }
+        
+        return true;
+    }
                     
     @Override
     public boolean doTest() 
     {
-        if (addPlayer(new Player(1, "Test", "User", "TU", "user@example.com")))
+        String email="user@example.com";
+        
+        if (addPlayer(new Player(1, "Test", "User", "TU", email)))
         {
             Player p = null;
             
@@ -139,6 +190,24 @@ public class DatabaseConnector implements Testable
             }
             
             System.out.println("Player P: " + p);
+        }
+        else  // Usually, Player is already in database.  BB: Should be an Exception.
+            return false;
+        
+        try
+        {
+            if (removePlayer(email))
+                System.out.println("Removed test player successfully.");
+            else
+            {
+                System.out.println("Could not remove the test player!");
+                return false;
+            }
+        }
+        catch(SQLException se)
+        {
+            se.printStackTrace();
+            return false;
         }
         
         return true;
